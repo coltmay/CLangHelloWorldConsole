1a3ae96217317918f89c6e6a97b12e52d878d5d3
Colten Mayberry <colten_mayberry@pm.me>
2023-05-22 19:10:05 -0500

    wrote simple loop to print name

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main() {
-    printf("Hello, World!");
+int main(void) {
+
+    char myName[7] = {'c', 'o', 'l', 't', 'e', 'n', '\0'};
+
+    printf("\nhello, world\nmy name is ");
+
+    for (int i = 0; i < sizeof(myName); ++i)
+    {
+        printf("%c", myName[i]);
+    }
+
     return 0;
 }
